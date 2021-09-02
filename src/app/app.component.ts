179571d26a7e7549e8c91dabcc624f1abef560e1
tajamul-hussain <tajamul.hussain@pec.edu>
2021-09-03 00:18:40 +0530

    added password length handler on input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'password';
+  password = "";
+  includeLetters=false;
+  includeSymbols=false;
+  includeNumbers=false;
+  length=0
+  onChangeLength(value:string)
+  {
+
+    const parsedval=parseInt(value)
+    if(!(parsedval===NaN))
+    {
+      this.length=parsedval;
+
+    }
+
+  }
+  onButtonClick() {
+    this.password = "myPassword";
+    console.log(this.includeLetters)
+    console.log(this.includeNumbers)
+    console.log(this.includeSymbols)
+  }
+  onChangeLetters(){
+    this.includeLetters=!this.includeLetters;
+
+  }
+  onChangeSymbols()
+  {
+    this.includeSymbols=!this.includeSymbols;
+  }
+  onchangeNumbers()
+  {
+    this.includeNumbers=!this.includeNumbers
+  }
 }
